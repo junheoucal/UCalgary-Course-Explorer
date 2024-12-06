3c3c71e8c7ec0316ee95dcf07ee1298f63b470d9
hillaryN08 <h.nguyenhillary@gmail.com>
2024-12-06 01:48:18 -0700

    My Degree page

--- client/src/studentpages/MyDegree.jsx
+++ client/src/studentpages/MyDegree.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../stylepages/StudentHome.css";
 
 const MyDegree = () => {
   return (
-    <div>
-      <h1>My Degree</h1>
-      <button>
-        <Link to="/studentpages/Major">View My Major</Link>
-      </button>
-      <button>
-        <Link to="/studentpages/Minor">View My Minor</Link>
-      </button>
+    <div className="ucalgary-container">
+      <div className="header">
+        <img
+          src="/uofc-logo.png"
+          alt="University of Calgary Logo"
+          className="ucalgary-logo"
+        />
+      </div>
+      <div className="student-home">
+        <h1>My Degree</h1>
+        <button className="student-button">
+          <Link to="/studentpages/Major">View My Major</Link>
+        </button>
+        <button className="student-button">
+          <Link to="/studentpages/Minor">View My Minor</Link>
+        </button>
+      </div>
     </div>
   );
 };
