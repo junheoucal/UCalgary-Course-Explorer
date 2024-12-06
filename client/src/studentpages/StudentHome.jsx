bf863cffffb84e9cb437670a7b20a561db253dea
hillaryN08 <h.nguyenhillary@gmail.com>
2024-12-06 01:34:19 -0700

    Student home page

--- client/src/studentpages/StudentHome.jsx
+++ client/src/studentpages/StudentHome.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
+import "../stylepages/StudentHome.css";
 
 const StudentHome = () => {
   const { auth } = useAuth();
   return (
     console.log(auth.UCID),
     (
-      <div>
+      <div className="ucalgary-container">
+        <div className="header">
+          <img
+            src="/uofc-logo.png"
+            alt="University of Calgary Logo"
+            className="ucalgary-logo"
+          />
+        </div>
+
         <h1>Student Home Page</h1>
         <div className="student-home">
-          <button>
+          <button className="student-button">
             <Link to="/studentpages/MyCourses">My Courses</Link>
           </button>
-          <button>
+          <button className="student-button">
             <Link to="/studentpages/CourseSearch">Course Search</Link>
           </button>
-          <button>
+          <button className="student-button">
             <Link to="/studentpages/MyDegree">My Degree</Link>
           </button>
         </div>
